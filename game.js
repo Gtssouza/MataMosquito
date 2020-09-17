b2df46571e2e333525ee636f7339f97d79b2f235
Gustavo Souza <gtssouza94@gmail.com>
2020-09-16 22:28:04 -0300

    Definindo dimensionamento da tela

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-alert("Jogo on")
+var largura = 0
+var altura = 0
+
+function ajustaTelaJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth  
+    console.log(altura, largura)  
+}
+
+ajustaTelaJogo()
+
+
+

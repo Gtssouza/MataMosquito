80e243eb50bd8aeddd1f60910b71df50a7a94f33
Gustavo Souza <gtssouza94@gmail.com>
2020-09-17 18:42:37 -0300

    Adicionando cronômetro

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var largura = 0
 var altura = 0
 var vidas = 1
+var tempo = 10
 
 function ajustaTelaJogo(){
     altura = window.innerHeight
@@ -9,6 +10,18 @@ function ajustaTelaJogo(){
 }
 
 ajustaTelaJogo()
+
+var cronometro = setInterval(function(){
+    tempo -= 1
+    if(tempo<0){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        alert('Vitoria')
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+}, 1000)
 
 function posicaoRandom(){
 
